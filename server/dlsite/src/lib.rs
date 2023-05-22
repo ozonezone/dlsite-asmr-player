709d118350dca4cc54f49aa15281760c40696e2a
ozonezone <132560491+ozonezone@users.noreply.github.com>
2023-05-22 18:00:52 +0900

    Add note

--- server/dlsite/src/lib.rs
+++ server/dlsite/src/lib.rs
@@ -1,6 +1,9 @@
-//! # dlsite-rs
+//! # [wip] dlsite-rs
 //! This is a library to get information about products on DLsite.
 //! Some information is not available on the HTML page, so this library also makes requests to the AJAX API.
+//!
+//! NOTE: This library is still wip, and the API may change.
+//! Also, only the parts I needed are implemented, so there are many unimplemented parts.
 
 use thiserror::Error;
 
