dcf052350d76c303b79761477fbf321b8b72eeda
ozonezone <132560491+ozonezone@users.noreply.github.com>
2023-07-09 22:08:34 +0900

    update

---
+++ server/dlsite/README.md
@@ -0,0 +1,12 @@
+# [wip] dlsite-rs
+
+This is a library to get information about products on DLsite. Some information
+is not available on the HTML page, so this library also makes requests to the
+AJAX API.
+
+NOTE: This library is still wip, and the API may change. Also, only the parts I
+needed are implemented, so there are many unimplemented parts.
+
+## Features
+
+- [x] Get product information.
--- server/dlsite/src/lib.rs
+++ server/dlsite/src/lib.rs
@@ -1,9 +1,4 @@
-//! # [wip] dlsite-rs
-//! This is a library to get information about products on DLsite.
-//! Some information is not available on the HTML page, so this library also makes requests to the AJAX API.
-//!
-//! NOTE: This library is still wip, and the API may change.
-//! Also, only the parts I needed are implemented, so there are many unimplemented parts.
+#![doc = include_str!("../README.md")]
 
 use thiserror::Error;
 
